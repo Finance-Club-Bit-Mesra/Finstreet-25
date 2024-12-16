4e9f77fd421b692d8b697840ec3701c47aa44a0f
VaibhavRaj001 <144055184+VaibhavRaj001@users.noreply.github.com>
2024-12-16 11:39:54 +0530

    Website

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    theme: {
+      extend: {
+        scrollBehavior: ['smooth'],
+      },
+    },
+    plugins: [],
+  };
+  
+  module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'fluorescentgreen': '#39FF14', // Custom fluorescent green color
+        },
+      },
+    },
+  }
